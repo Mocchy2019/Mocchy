9313858df2a0ad62bbea28509bbb299585045058
Taiga Ito <>
2019-02-26 17:27:22 +0900

    cssをホームページに反映した(jsは×)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( master.js )
+Rails.application.config.assets.precompile += %w( master.css )
+Rails.application.config.assets.precompile += %w( slide-show.js )
+Rails.application.config.assets.precompile += %w( unit.css )
