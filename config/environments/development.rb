e6daeddff202da4b269170f4f339846170de2c37
Taiga Ito <>
2019-03-01 18:13:20 +0900

    Fixed js precompile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,11 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations
+
   # config.action_view.raise_on_missing_translations = true
+
+  # precompile (development)
+  config.assets.precompile += ['master.js']
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
